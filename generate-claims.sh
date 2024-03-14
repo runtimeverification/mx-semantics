91d1cc24659ad015457ddcf0bdc926e5844f5d82
Virgil <25692529+virgil-serbanuta@users.noreply.github.com>
2024-03-14 11:43:25 +0200

    Update the mx libraries (#214)
    * Update the mx libraries
    
    * Set Version: 0.1.15
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- generate-claims.sh
+++ generate-claims.sh
@@ -2,10 +2,12 @@
 
 set -e -o pipefail
 
+poetry -C kmultiversx install
+
 # Adder
 
-mxpy contract build --path deps/mx-sdk-rs/contracts/examples/adder --wasm-symbols --no-wasm-opt
-mxpy contract build --path tests/contracts/foundrylike --wasm-symbols --no-wasm-opt
+sc-meta all build --path deps/mx-sdk-rs/contracts/examples/adder --wasm-symbols --no-wasm-opt
+sc-meta all build --path tests/contracts/foundrylike --wasm-symbols --no-wasm-opt
 K_OPTS="-Xmx8192m" kbuild kompile llvm-kasmer && poetry -C kmultiversx run -- kasmer --definition-dir $(kbuild which llvm-kasmer) --directory "tests/contracts/foundrylike" --gen-claims 2>&1 | tee kasmer.log
 
 # Coindrip
@@ -22,17 +24,17 @@ K_OPTS="-Xmx8192m" kbuild kompile llvm-kasmer && poetry -C kmultiversx run -- ka
 # Crowdfunding
 
 mxpy contract build --path deps/mx-sdk-rs/contracts/examples/crowdfunding-esdt --wasm-symbols --no-wasm-opt
-mxpy contract build --path tests/contracts/test_crowdfunding-esdt --wasm-symbols --no-wasm-opt
+sc-meta all build --path tests/contracts/test_crowdfunding-esdt --wasm-symbols --no-wasm-opt
 K_OPTS="-Xmx8192m" kbuild kompile llvm-kasmer && poetry -C kmultiversx run -- kasmer --definition-dir $(kbuild which llvm-kasmer) --directory "tests/contracts/test_crowdfunding-esdt" --gen-claims 2>&1 | tee kasmer.log
 
 # Pair
 
 mxpy contract build --path deps/mx-exchange-sc/dex/pair --wasm-symbols --no-wasm-opt
-mxpy contract build --path tests/contracts/test_pair --wasm-symbols --no-wasm-opt
+sc-meta all build --path tests/contracts/test_pair --wasm-symbols --no-wasm-opt
 K_OPTS="-Xmx8192m" kbuild kompile llvm-kasmer && poetry -C kmultiversx run -- kasmer --definition-dir $(kbuild which llvm-kasmer) --directory "tests/contracts/test_pair" --gen-claims 2>&1 | tee kasmer.log
 
 # Multisig
 
 mxpy contract build --path deps/mx-sdk-rs/contracts/examples/multisig --wasm-symbols --no-wasm-opt
-mxpy contract build --path tests/contracts/test_multisig --wasm-symbols --no-wasm-opt
+sc-meta all build --path tests/contracts/test_multisig --wasm-symbols --no-wasm-opt
 K_OPTS="-Xmx8192m" kbuild kompile llvm-kasmer && poetry -C kmultiversx run -- kasmer --definition-dir $(kbuild which llvm-kasmer) --directory "tests/contracts/test_multisig" --gen-claims 2>&1 | tee kasmer.log

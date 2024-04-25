ac315605fddbf196bc47853ea126d4a586b3b800
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-04-25 15:10:56 +0000

    Add target `PluginTarget`

--- kmultiversx/src/kmultiversx/kdist/plugin.py
+++ kmultiversx/src/kmultiversx/kdist/plugin.py
@@ -1,20 +1,18 @@
 from __future__ import annotations
 
 import shutil
+from distutils.dir_util import copy_tree
 from pathlib import Path
 from typing import TYPE_CHECKING
 
-from pyk.kbuild.utils import k_version
+from pyk.kbuild.utils import k_version, sync_files
 from pyk.kdist.api import Target
 from pyk.ktool.kompile import KompileBackend, kompile
+from pyk.utils import run_process
 
 if TYPE_CHECKING:
     from collections.abc import Callable, Mapping
     from typing import Any, Final
-
-
-CURRENT_DIR: Final = Path(__file__).parent
-PLUGIN_DIR: Final = CURRENT_DIR / 'plugin'
 
 
 class SourceTarget(Target):
@@ -32,6 +30,35 @@ class SourceTarget(Target):
         return ('wasm-semantics.source',)
 
 
+class PluginTarget(Target):
+    PLUGIN_DIR: Final = Path(__file__).parent / 'plugin'
+
+    def build(self, output_dir: Path, deps: dict[str, Any], args: dict[str, Any], verbose: bool) -> None:
+        sync_files(
+            source_dir=self.PLUGIN_DIR / 'plugin-c',
+            target_dir=output_dir / 'plugin-c',
+            file_names=[
+                'blake2.h',
+                'crypto.cpp',
+                'plugin_util.cpp',
+                'plugin_util.h',
+            ],
+        )
+
+        copy_tree(str(self.PLUGIN_DIR), '.')
+        run_process(
+            ['make', 'libcryptopp', 'libff', 'blake2', '-j8'],
+            pipe_stdout=not verbose,
+        )
+
+        copy_tree('./build/libcryptopp', str(output_dir / 'libcryptopp'))
+        copy_tree('./build/libff', str(output_dir / 'libff'))
+        copy_tree('./build/blake2', str(output_dir / 'blake2'))
+
+    def source(self) -> tuple[Path]:
+        return (self.PLUGIN_DIR,)
+
+
 class KompileTarget(Target):
     _kompile_args: Callable[[Path], Mapping[str, Any]]
 
@@ -40,19 +67,19 @@ class KompileTarget(Target):
 
     def build(self, output_dir: Path, deps: dict[str, Path], args: dict[str, Any], verbose: bool) -> None:
         # TODO Pass K_OPTS='-Xmx8G -Xss512m'
-        kompile_args = self._kompile_args(deps['mx-semantics.source'])
+        kompile_args = self._kompile_args(deps['mx-semantics.source'], deps['mx-semantics.plugin'])
         kompile(output_dir=output_dir, verbose=verbose, **kompile_args)
 
     def context(self) -> dict[str, str]:
         return {'k-version': k_version().text}
 
-    def deps(self) -> tuple[str]:
-        return ('mx-semantics.source',)
+    def deps(self) -> tuple[str, str]:
+        return ('mx-semantics.source', 'mx-semantics.plugin')
 
 
 def llvm_target(main_file_name: str, main_module: str, syntax_module: str) -> KompileTarget:
     return KompileTarget(
-        lambda src_dir: {
+        lambda src_dir, plugin_dir: {
             'backend': KompileBackend.LLVM,
             'main_file': src_dir / 'mx-semantics' / main_file_name,
             'main_module': main_module,
@@ -68,14 +95,14 @@ def llvm_target(main_file_name: str, main_module: str, syntax_module: str) -> Ko
                 '-lprocps',
                 '-lsecp256k1',
                 '-lssl',
-                f"{PLUGIN_DIR / 'build/blake2/lib/blake2.a'}",
-                f"-I{PLUGIN_DIR / 'build/blake2/include'}",
-                f"{PLUGIN_DIR / 'build/libcryptopp/lib/libcryptopp.a'}",
-                f"-I{PLUGIN_DIR / 'build/libcryptopp/include'}",
-                f"{PLUGIN_DIR / 'build/libff/lib/libff.a'}",
-                f"-I{PLUGIN_DIR / 'build/libff/include'}",
-                f"{PLUGIN_DIR / 'plugin-c/crypto.cpp'}",
-                f"{PLUGIN_DIR / 'plugin-c/plugin_util.cpp'}",
+                str(plugin_dir / 'blake2/lib/blake2.a'),
+                f"-I{plugin_dir / 'blake2/include'}",
+                str(plugin_dir / 'libcryptopp/lib/libcryptopp.a'),
+                f"-I{plugin_dir / 'libcryptopp/include'}",
+                str(plugin_dir / 'libff/lib/libff.a'),
+                f"-I{plugin_dir / 'libff/include'}",
+                str(plugin_dir / 'plugin-c/crypto.cpp'),
+                str(plugin_dir / 'plugin-c/plugin_util.cpp'),
             ],
         },
     )
@@ -83,7 +110,7 @@ def llvm_target(main_file_name: str, main_module: str, syntax_module: str) -> Ko
 
 def haskell_target(main_file_name: str, main_module: str, syntax_module: str) -> KompileTarget:
     return KompileTarget(
-        lambda src_dir: {
+        lambda src_dir, plugin_dir: {
             'backend': KompileBackend.HASKELL,
             'main_file': src_dir / 'mx-semantics' / main_file_name,
             'main_module': main_module,
@@ -98,6 +125,7 @@ def haskell_target(main_file_name: str, main_module: str, syntax_module: str) ->
 
 __TARGETS__: Final = {
     'source': SourceTarget(),
+    'plugin': PluginTarget(),
     'llvm-mandos': llvm_target('mandos.md', 'MANDOS', 'MANDOS-SYNTAX'),
     'llvm-kasmer': llvm_target('kasmer.md', 'KASMER', 'KASMER-SYNTAX'),
     'haskell-mandos': haskell_target('mandos.md', 'MANDOS', 'MANDOS-SYNTAX'),

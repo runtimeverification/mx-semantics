e05964eb1a6e0b28e98c922a8f0c1289a7be5c9b
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-04-26 19:42:20 +0200

    Add target `mx-semantics.plugin`  (#243)
    * Move the `plugin` submodule into the Python package
    
    * Copy `krypto.md` into `mx-semantics.source`
    
    * Add target `PluginTarget`
    
    * Set Version: 0.1.42
    
    * Set Version: 0.1.43
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>
    Co-authored-by: rv-jenkins <admin@runtimeverification.com>

--- kmultiversx/src/kmultiversx/kdist/plugin.py
+++ kmultiversx/src/kmultiversx/kdist/plugin.py
@@ -1,20 +1,18 @@
 from __future__ import annotations
 
 import shutil
+from distutils.dir_util import copy_tree
 from pathlib import Path
 from typing import TYPE_CHECKING
 
-from pyk.kbuild.utils import k_version
+from pyk.kbuild.utils import k_version, sync_files
 from pyk.kdist.api import Target
 from pyk.ktool.kompile import KompileBackend, kompile
+from pyk.utils import run_process
 
 if TYPE_CHECKING:
     from collections.abc import Callable, Mapping
     from typing import Any, Final
-
-
-CURRENT_DIR: Final = Path(__file__).parent
-PLUGIN_DIR: Final = CURRENT_DIR.parents[3] / 'deps/plugin'  # TODO Distribute plugin files with Python
 
 
 class SourceTarget(Target):
@@ -22,6 +20,7 @@ class SourceTarget(Target):
 
     def build(self, output_dir: Path, deps: dict[str, Path], args: dict[str, Any], verbose: bool) -> None:
         shutil.copytree(deps['wasm-semantics.source'] / 'wasm-semantics', output_dir / 'wasm-semantics')
+        shutil.copytree(self.SRC_DIR / 'plugin/plugin', output_dir / 'plugin')
         shutil.copytree(self.SRC_DIR / 'mx-semantics', output_dir / 'mx-semantics')
 
     def source(self) -> tuple[Path, ...]:
@@ -29,6 +28,35 @@ class SourceTarget(Target):
 
     def deps(self) -> tuple[str]:
         return ('wasm-semantics.source',)
+
+
+class PluginTarget(Target):
+    PLUGIN_DIR: Final = Path(__file__).parent / 'plugin'
+
+    def build(self, output_dir: Path, deps: dict[str, Any], args: dict[str, Any], verbose: bool) -> None:
+        sync_files(
+            source_dir=self.PLUGIN_DIR / 'plugin-c',
+            target_dir=output_dir / 'plugin-c',
+            file_names=[
+                'blake2.h',
+                'crypto.cpp',
+                'plugin_util.cpp',
+                'plugin_util.h',
+            ],
+        )
+
+        copy_tree(str(self.PLUGIN_DIR), '.')
+        run_process(
+            ['make', 'libcryptopp', 'libff', 'blake2', '-j8'],
+            pipe_stdout=not verbose,
+        )
+
+        copy_tree('./build/libcryptopp', str(output_dir / 'libcryptopp'))
+        copy_tree('./build/libff', str(output_dir / 'libff'))
+        copy_tree('./build/blake2', str(output_dir / 'blake2'))
+
+    def source(self) -> tuple[Path]:
+        return (self.PLUGIN_DIR,)
 
 
 class KompileTarget(Target):
@@ -39,24 +67,24 @@ class KompileTarget(Target):
 
     def build(self, output_dir: Path, deps: dict[str, Path], args: dict[str, Any], verbose: bool) -> None:
         # TODO Pass K_OPTS='-Xmx8G -Xss512m'
-        kompile_args = self._kompile_args(deps['mx-semantics.source'])
+        kompile_args = self._kompile_args(deps['mx-semantics.source'], deps['mx-semantics.plugin'])
         kompile(output_dir=output_dir, verbose=verbose, **kompile_args)
 
     def context(self) -> dict[str, str]:
         return {'k-version': k_version().text}
 
-    def deps(self) -> tuple[str]:
-        return ('mx-semantics.source',)
+    def deps(self) -> tuple[str, str]:
+        return ('mx-semantics.source', 'mx-semantics.plugin')
 
 
 def llvm_target(main_file_name: str, main_module: str, syntax_module: str) -> KompileTarget:
     return KompileTarget(
-        lambda src_dir: {
+        lambda src_dir, plugin_dir: {
             'backend': KompileBackend.LLVM,
             'main_file': src_dir / 'mx-semantics' / main_file_name,
             'main_module': main_module,
             'syntax_module': syntax_module,
-            'include_dirs': [src_dir, PLUGIN_DIR],
+            'include_dirs': [src_dir],
             'md_selector': 'k',
             'hook_namespaces': ['KRYPTO'],
             'opt_level': 2,
@@ -67,14 +95,14 @@ def llvm_target(main_file_name: str, main_module: str, syntax_module: str) -> Ko
                 '-lprocps',
                 '-lsecp256k1',
                 '-lssl',
-                f"{PLUGIN_DIR / 'build/blake2/lib/blake2.a'}",
-                f"-I{PLUGIN_DIR / 'build/blake2/include'}",
-                f"{PLUGIN_DIR / 'build/libcryptopp/lib/libcryptopp.a'}",
-                f"-I{PLUGIN_DIR / 'build/libcryptopp/include'}",
-                f"{PLUGIN_DIR / 'build/libff/lib/libff.a'}",
-                f"-I{PLUGIN_DIR / 'build/libff/include'}",
-                f"{PLUGIN_DIR / 'plugin-c/crypto.cpp'}",
-                f"{PLUGIN_DIR / 'plugin-c/plugin_util.cpp'}",
+                str(plugin_dir / 'blake2/lib/blake2.a'),
+                f"-I{plugin_dir / 'blake2/include'}",
+                str(plugin_dir / 'libcryptopp/lib/libcryptopp.a'),
+                f"-I{plugin_dir / 'libcryptopp/include'}",
+                str(plugin_dir / 'libff/lib/libff.a'),
+                f"-I{plugin_dir / 'libff/include'}",
+                str(plugin_dir / 'plugin-c/crypto.cpp'),
+                str(plugin_dir / 'plugin-c/plugin_util.cpp'),
             ],
         },
     )
@@ -82,12 +110,12 @@ def llvm_target(main_file_name: str, main_module: str, syntax_module: str) -> Ko
 
 def haskell_target(main_file_name: str, main_module: str, syntax_module: str) -> KompileTarget:
     return KompileTarget(
-        lambda src_dir: {
+        lambda src_dir, plugin_dir: {
             'backend': KompileBackend.HASKELL,
             'main_file': src_dir / 'mx-semantics' / main_file_name,
             'main_module': main_module,
             'syntax_module': syntax_module,
-            'include_dirs': [src_dir, PLUGIN_DIR],
+            'include_dirs': [src_dir],
             'md_selector': 'k',
             'hook_namespaces': ['KRYPTO'],
             'warnings_to_errors': True,
@@ -97,6 +125,7 @@ def haskell_target(main_file_name: str, main_module: str, syntax_module: str) ->
 
 __TARGETS__: Final = {
     'source': SourceTarget(),
+    'plugin': PluginTarget(),
     'llvm-mandos': llvm_target('mandos.md', 'MANDOS', 'MANDOS-SYNTAX'),
     'llvm-kasmer': llvm_target('kasmer.md', 'KASMER', 'KASMER-SYNTAX'),
     'haskell-mandos': haskell_target('mandos.md', 'MANDOS', 'MANDOS-SYNTAX'),

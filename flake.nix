6a161adedd3cb781b4c5df9f6e48e4fc78b4872c
Bruce Collie <brucecollie82@gmail.com>
2024-06-27 10:49:48 +0100

    Fix hypothesis issue

--- flake.nix
+++ flake.nix
@@ -114,12 +114,6 @@
             (finalPython: prevPython: {
               pyk = nixpkgs-pyk.pyk-python310;
               pykwasm = wasm-semantics.packages.${prev.system}.kwasm-pyk;
-              hypothesis = prevPython.hypothesis.overridePythonAttrs (old: {
-                  propagatedBuildInputs = prev.lib.filter
-                    (x: !(prev.lib.strings.hasInfix "attrs" x.name || prev.lib.strings.hasInfix "exceptiongroup" x.name))
-                    old.propagatedBuildInputs;
-                  buildInputs = (old.buildInputs or []) ++ [ finalPython.attrs finalPython.exceptiongroup ];
-              });
             });
             groups = [ ];
             checkGroups = [ ];

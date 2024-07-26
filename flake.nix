b68f34001df2c47280c129ea97d2ee393ee5add8
rv-jenkins <admin@runtimeverification.com>
2024-07-26 03:24:57 -0600

    Update dependency: kmultiversx/src/kmultiversx/kdist/plugin (#312)
    * kmultiversx/src/kmultiversx/kdist/plugin: Set Version f4c41bfa5e44f5bc29e5d9151557ce735bcde087
    
    * Set Version: 0.1.102
    
    * kmultiversx/: sync poetry files 0.1.90
    
    * deps/blockchain-k-plugin_release: sync release file version f4c41bfa5e44f5bc29e5d9151557ce735bcde087
    
    * flake.{nix,lock}: update Nix derivations
    
    ---------
    
    Co-authored-by: devops <devops@runtimeverification.com>

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     poetry2nix.follows = "pyk/poetry2nix";
     blockchain-k-plugin = {
       url =
-        "github:runtimeverification/blockchain-k-plugin/8031e7be727e903b817fff6639a9d3a4bbe82f11";
+        "github:runtimeverification/blockchain-k-plugin/f4c41bfa5e44f5bc29e5d9151557ce735bcde087";
       inputs.flake-utils.follows = "k-framework/flake-utils";
       inputs.nixpkgs.follows = "k-framework/nixpkgs";
     };

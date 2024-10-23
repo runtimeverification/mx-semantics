dd9a9969697b27e8205943452c0f443e676f3133
devops <devops@runtimeverification.com>
2024-10-23 14:29:18 +0000

    flake.{nix,lock}: update Nix derivations

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "K Semantics of MultiversX";
 
   inputs = {
-    wasm-semantics.url = "github:runtimeverification/wasm-semantics/v0.1.111";
+    wasm-semantics.url = "github:runtimeverification/wasm-semantics/v0.1.112";
     k-framework.follows = "wasm-semantics/k-framework";
     nixpkgs.follows = "k-framework/nixpkgs";
     flake-utils.follows = "k-framework/flake-utils";
